9d2444dcbb776950226937c81d10ff2fce488158
Guilherme Freschi <guilhermefreschix@gmail.com>
2025-04-08 22:29:27 -0300

    feat: Implement events_repository

---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,27 @@
+import pytest
+from src.models.settings.connection import db_connection_handler
+from .events_repository import EventsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Novo registro em Banco de Dados.")
+def test_inseret_events():
+    event = {
+        "uuid": "meu-uuid",
+        "title": "meu title",
+        "slug": "Meu Slog Aqui",
+        "maximum_attendees": 20
+    }
+
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event)
+    print(response)
+
+@pytest.mark.skip(reason="Não necessita.")
+def test_get_event_by_id():
+    event_id = 'meu-uuid'
+
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+
+    print(response)
