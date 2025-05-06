e29f6be019a315ad0e730139785a70b2d2d5906e
Guilherme Freschi <guilhermefreschix@gmail.com>
2025-05-05 21:03:51 -0300

    improve: Add try in events repo

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -5,7 +5,7 @@ from .events_repository import EventsRepository
 db_connection_handler.connect_to_db()
 
 @pytest.mark.skip(reason="Novo registro em Banco de Dados.")
-def test_inseret_events():
+def test_insert_events():
     event = {
         "uuid": "meu-uuid",
         "title": "meu title",
@@ -17,9 +17,9 @@ def test_inseret_events():
     response = events_repository.insert_event(event)
     print(response)
 
-@pytest.mark.skip(reason="Não necessita.")
+# @pytest.mark.skip(reason="Não necessita.")
 def test_get_event_by_id():
-    event_id = 'meu-uuid'
+    event_id = 'meu-uuid-12312'
 
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
